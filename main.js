5544c6862c12a990666ef20cc60c933c4785a5a2
Genesis Lara <genesislara@pursuit.org>
2023-07-10 22:36:47 -0400

    working on js file

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-const BASE_URL = "https://rickandmortyapi.com/api";
+const BASE_URL = "https://rickandmortyapi.com/api/character/1";
 
 const button = document.querySelector("button");
+
+function generateNewCharacter({results}) {
+  const newCharacter = results;
+  const section = document.createElement("section");
+  
+  const img = document.createElement("img");
+  img.setAttribute("src", newCharacter.image);
+  
+  const h2 = document.createElement("h2");
+  h2.textContent = newCharacter.name;
+
+  section.append(img,h2)
+  document.querySelector(".characters").append(section);
+}
+
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
   fetch(`${BASE_URL}`)
     .then(data => data.json())
-   // .then(json =>)
+    .then(json => generateNewCharacter(json))
+    .catch(displayErr);
 })
