ffa6679c0c718ba72d132b37753b1fc76ea6ff51
Genesis Lara <genesislara@pursuit.org>
2023-07-15 10:46:21 -0400

    it works

--- generator-docs/generatorpage.js
+++ generator-docs/generatorpage.js
@@ -2,30 +2,32 @@ const BASE_URL = "https://rickandmortyapi.com/api/character";
 
 const button = document.querySelector("#submit");
 const form = document.querySelector("#new-character");
-const div = document.querySelector("div");
+const div = document.querySelector(".image-container");
 
 function generateNewCharacter(results) {
   const newCharacter = results;
+  console.log(newCharacter)
   const searchValue = document.getElementById("name").value;
 
   for (let i = 0; i < newCharacter.results.length; i++) {
     const character = newCharacter.results[i];
     const firstName = character.name.split(" ")[0]; 
+
     if (firstName === searchValue) {
-      let image = character.image;
       const section = document.createElement("section");
       const img = document.createElement("img");
+      let image = character.image;
+      console.log(image)
       img.setAttribute("src", image);
       img.setAttribute("alt", searchValue);
       const h2 = document.createElement("h2");
       h2.textContent = character.name;
 
-      div.innerHTML = '';
       div.append(h2, img);
       div.append(section);
-      return ;
   }
-  div.innerHTML = '<p>No matching character found.</p>';
+  //div.innerHTML = '<p>No matching character found.</p>';
+}
 }
 
 form.addEventListener("submit", (e) => {
@@ -36,4 +38,3 @@ form.addEventListener("submit", (e) => {
       generateNewCharacter(data);
     });
 });
-}
