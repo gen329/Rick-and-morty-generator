4fd71780c69b6f4dd0e6a53a4940a862b8e5c810
Genesis Lara <genesislara@pursuit.org>
2023-07-15 09:55:20 -0400

    new fonts

--- generator-docs/generatorpage.js
+++ generator-docs/generatorpage.js
@@ -2,32 +2,38 @@ const BASE_URL = "https://rickandmortyapi.com/api/character";
 
 const button = document.querySelector("#submit");
 const form = document.querySelector("#new-character");
-const div = document.querySelector("div")
-
+const div = document.querySelector("div");
 
 function generateNewCharacter(results) {
   const newCharacter = results;
-  let image = newCharacter.results[0].image;
-  const section = document.createElement("section");
-  const img = document.createElement("img");
-  img.setAttribute("src", `${image}`);
-  img.setAttribute("alt","name");
-  const h2 = document.createElement("h2");
-  h2.textContent = newCharacter.name;
-  div.append(h2, img)
+  const searchValue = document.getElementById("name").value;
 
-  const characters = document.querySelector(".characters")
-  div.append(section);
+  for (let i = 0; i < newCharacter.results.length; i++) {
+    const character = newCharacter.results[i];
+    const firstName = character.name.split(" ")[0]; 
+    if (firstName === searchValue) {
+      let image = character.image;
+      const section = document.createElement("section");
+      const img = document.createElement("img");
+      img.setAttribute("src", image);
+      img.setAttribute("alt", searchValue);
+      const h2 = document.createElement("h2");
+      h2.textContent = character.name;
+
+      div.innerHTML = '';
+      div.append(h2, img);
+      div.append(section);
+      return ;
+  }
+  div.innerHTML = '<p>No matching character found.</p>';
 }
- 
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(BASE_URL)
-  .then(data => data.json())
-  .then((json) => {
-      generateNewCharacter(json)
-
-    })
-
-})
+    .then((response) => response.json())
+    .then((data) => {
+      generateNewCharacter(data);
+    });
+});
+}
